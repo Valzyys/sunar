6c8d90f5ffaf8008829ca7eda5a6d73d63dcc8b0
Valzz <jkt48connect@gmail.com>
2026-03-18 14:51:29 +0700

    Update logo.tsx

--- apps/docs/src/components/logo.tsx
+++ apps/docs/src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
     return (
         <div title="Sunar" className="flex items-center gap-x-2 text-stone-800 dark:text-stone-100">
             <SunarIcon {...props} />
-            <span className="font-medium text-xl">Sunar</span>
+            <span className="font-medium text-xl">JKT48Connect</span>
         </div>
     );
 }
